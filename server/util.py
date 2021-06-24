792459994c605f9beb3748f26344b87ce0e27248
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-06-23 23:04:14 -0400

    Add files via upload

---
+++ server/util.py
@@ -0,0 +1,11 @@
+from os import symlink
+
+
+def read(filepath):
+    f = open(filepath, 'r')
+    lines = []
+    raw = f.readlines()
+    for line in raw:
+        line = line.replace("\n", "")
+        lines.append(line)
+    return lines
