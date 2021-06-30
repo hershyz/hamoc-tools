0789a99d9895421e5ef3ab721429d10610b44557
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-06-30 18:20:54 -0400

    add test

--- server/endpoint_tests/option_query.py
+++ server/endpoint_tests/option_query.py
@@ -1,9 +1,9 @@
 import requests
 
 payload = {
-    "low": 1,
-    "high": 1,
-    "property": "bab"
+    "low": 300,
+    "high": 400,
+    "property": "strike"
 }
 
 r = requests.get("http://127.0.0.1:5000/option_query", json=payload)
