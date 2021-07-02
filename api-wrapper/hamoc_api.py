aeb7d58c2e7100fbac94cfbf2cac33f532a30bc2
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-07-01 23:54:29 -0400

    add symbols wrapper

--- api-wrapper/hamoc_api.py
+++ api-wrapper/hamoc_api.py
@@ -1,9 +1,14 @@
 import requests
 
+
 class HamocClient:    
     
     def __init__(self, uri):
         self.uri = uri
     
     def add_symbols(self, symbols):
-        r = requests.post("/add_symbols")
+        payload = {
+            "symbols": symbols
+        }
+        r = requests.post(self.uri + "/add_symbols", json=payload)
+        return r.text
---
+++ api-wrapper/test_client.py
@@ -0,0 +1,5 @@
+import hamoc_api as hamocapi
+
+# add symbols
+# client = hamocapi.HamocClient("http://127.0.0.1:5000")
+# print(client.add_symbols(["ibm", "amzn"]))
