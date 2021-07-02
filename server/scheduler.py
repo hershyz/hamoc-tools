277be51347bded507037a39c3f61436ec335568d
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-07-01 21:13:48 -0400

    make scheduler

---
+++ server/scheduler.py
@@ -0,0 +1,10 @@
+import time
+import requests
+
+def init(uri):
+    while True:
+        stock_request = requests.get(uri + "/stock_store")
+        options_request = requests.get(uri + "/options_store")
+        print("[scheduler]: " + stock_request.text)
+        print("[scheduler]: " + options_request.text)
+        time.sleep(43200) # 432000 = 12 hours -> update options and data twice a day (on server init)
